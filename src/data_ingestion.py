5ad69cf8efdddfe80fb7fb15a44bbcc35c570616
kurekhombre <kurekhombre@gmail.com>
2024-07-15 21:18:02 +0200

    Now??

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -8,7 +8,7 @@ import logging
 
 load_dotenv('secrets/.env')
 
-ACCOUNT_SERVICE_KEY = os.environ['GOOGLECLOUDSERVICEKEY']
+ACCOUNT_SERVICE_KEY = os.environ['ACCOUNT_SERVICE_KEY']
 GCS_BUCKET_NAME = os.environ['GCS_BUCKET_NAME']
 OPENWEATHER_API_KEY = os.environ['OPENWEATHER_API_KEY']
 
