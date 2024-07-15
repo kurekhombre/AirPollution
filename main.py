5260441f64f78fa74ad51dbab5f59879e6eccada
kurekhombre <kurekhombre@gmail.com>
2024-07-15 20:56:41 +0200

    Test4

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-# import logging
-# from src.data_ingestion import fetch_city_coordinates, fetch_air_pollution_data, upload_to_gcs, add_metadata_to_data, generate_filename
-# import os
-# from dotenv import load_dotenv
-# import yaml
-# from flask import jsonify, request
-# from google.cloud import storage
+import logging
+from src.data_ingestion import fetch_city_coordinates, fetch_air_pollution_data, upload_to_gcs, add_metadata_to_data, generate_filename
+import os
+from dotenv import load_dotenv
+import yaml
+from flask import jsonify, request
+from google.cloud import storage
 import functions_framework
 
 
@@ -24,78 +24,77 @@ import functions_framework
 #   "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/airpollution%40able-store-371110.iam.gserviceaccount.com",
 #   "universe_domain": "googleapis.com"
 # }
-# GCS_BUCKET_NAME = os.environ['GCS_BUCKET_NAME']
+# GCS_BUCKET_NAME = 'airpollution_bucket'
 # OPENWEATHER_API_KEY = '90da9cd29152fb94e06a1ec2f90d7557'
 
-# def process_data_extraction(city_name, config):
-#     # OPENWEATHER_API_KEY = config['openweather']['api_key']
-#     # GCS_BUCKET_NAME = config['gcs']['bucket_name']
-#     # ACCOUNT_SERVICE_KEY = config['gcs']['account_service_key']
+def process_data_extraction(city_name, config):
+    OPENWEATHER_API_KEY = config['openweather']['api_key']
+    GCS_BUCKET_NAME = config['gcs']['bucket_name']
+    ACCOUNT_SERVICE_KEY = config['gcs']['account_service_key']
     
-#     lat, lon = fetch_city_coordinates(city_name, OPENWEATHER_API_KEY)
-#     pollution_data = fetch_air_pollution_data(lat, lon, OPENWEATHER_API_KEY)
+    lat, lon = fetch_city_coordinates(city_name, OPENWEATHER_API_KEY)
+    pollution_data = fetch_air_pollution_data(lat, lon, OPENWEATHER_API_KEY)
 
-#     pollution_data_with_metadata = add_metadata_to_data(pollution_data, city_name) if config['output']['metadata'] else pollution_data
+    pollution_data_with_metadata = add_metadata_to_data(pollution_data, city_name) if config['output']['metadata'] else pollution_data
 
-#     file_name = generate_filename(city_name)
+    file_name = generate_filename(city_name)
 
-#     upload_to_gcs(ACCOUNT_SERVICE_KEY, GCS_BUCKET_NAME, file_name, pollution_data_with_metadata)
+    upload_to_gcs(ACCOUNT_SERVICE_KEY, GCS_BUCKET_NAME, file_name, pollution_data_with_metadata)
     
-#     logging.info("Data ingestion and upload process completed successfully.")
+    logging.info("Data ingestion and upload process completed successfully.")
     
-#     response_data = {
-#         "city_name": city_name,
-#         "coordinates": {
-#             "latitude": lat,
-#             "longitude": lon
-#         },
-#         "pollution_data": pollution_data,
-#         "pollution_data_with_metadata": pollution_data_with_metadata,
-#         "file_name": file_name
-#     }
+    response_data = {
+        "city_name": city_name,
+        "coordinates": {
+            "latitude": lat,
+            "longitude": lon
+        },
+        "pollution_data": pollution_data,
+        "pollution_data_with_metadata": pollution_data_with_metadata,
+        "file_name": file_name
+    }
 
-#     return response_data
+    return response_data
 
-# with open('config.yaml', 'r') as config_file:
-#     config = yaml.safe_load(config_file)
+with open('config.yaml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
 
 @functions_framework.http
 def open_weather_data_extract(request, context=None):
-    return "Hello Reciutki"
-    # try:
-    #     request_json = request.get_json(silent=True)
-    #     if request_json and 'city_name' in request_json:
-    #         city_name = request_json['city_name']
-    #     else:
-    #         return jsonify({"error": "City name not provided"}), 400
+    try:
+        request_json = request.get_json(silent=True)
+        if request_json and 'city_name' in request_json:
+            city_name = request_json['city_name']
+        else:
+            return jsonify({"error": "City name not provided"}), 400
 
-    #     lat, lon = fetch_city_coordinates(city_name, OPENWEATHER_API_KEY)
-    #     pollution_data = fetch_air_pollution_data(lat, lon, OPENWEATHER_API_KEY)
+        lat, lon = fetch_city_coordinates(city_name, OPENWEATHER_API_KEY)
+        pollution_data = fetch_air_pollution_data(lat, lon, OPENWEATHER_API_KEY)
 
-    #     pollution_data_with_metadata = add_metadata_to_data(pollution_data, city_name) if config['output']['metadata'] else pollution_data
+        pollution_data_with_metadata = add_metadata_to_data(pollution_data, city_name) if config['output']['metadata'] else pollution_data
 
-    #     file_name = generate_filename(city_name)
+        file_name = generate_filename(city_name)
 
-    #     upload_to_gcs(ACCOUNT_SERVICE_KEY, GCS_BUCKET_NAME, file_name, pollution_data_with_metadata)
+        upload_to_gcs(ACCOUNT_SERVICE_KEY, GCS_BUCKET_NAME, file_name, pollution_data_with_metadata)
         
-    #     logging.info("Data ingestion and upload process completed successfully.")
+        logging.info("Data ingestion and upload process completed successfully.")
         
-    #     response_data = {
-    #         "city_name": city_name,
-    #         "coordinates": {
-    #             "latitude": lat,
-    #             "longitude": lon
-    #         },
-    #         "pollution_data": pollution_data,
-    #         "pollution_data_with_metadata": pollution_data_with_metadata,
-    #         "file_name": file_name
-    #     }
+        response_data = {
+            "city_name": city_name,
+            "coordinates": {
+                "latitude": lat,
+                "longitude": lon
+            },
+            "pollution_data": pollution_data,
+            "pollution_data_with_metadata": pollution_data_with_metadata,
+            "file_name": file_name
+        }
 
-    #     return jsonify(response_data), 200
+        return jsonify(response_data), 200
 
-    # except Exception as e:
-    #     logging.error(f"An error occurred: {e}")
-    #     return jsonify({"error": f"An error occurred: {e}"}), 500
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        return jsonify({"error": f"An error occurred: {e}"}), 500
 
 # # import logging
 # # from src.data_ingestion import fetch_city_coordinates, fetch_air_pollution_data, upload_to_gcs, add_metadata_to_data, generate_filename
