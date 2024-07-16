67943ebae07d1d2007dc1a8e701974ce80c994dd
kurekhombre <kurekhombre@gmail.com>
2024-07-16 19:49:51 +0200

    Finally!

---
+++ gcloud_functions/main.py
@@ -0,0 +1,52 @@
+import logging
+from gcloud_functions.utils.data_ingestion import fetch_city_coordinates, fetch_air_pollution_data, upload_to_gcs, add_metadata_to_data, generate_filename
+import os
+from dotenv import load_dotenv
+import yaml
+from flask import jsonify, request
+from google.cloud import storage
+import functions_framework
+
+
+OPENWEATHER_API_KEY=os.environ['OPENWEATHER_API_KEY']
+
+
+with open('config.yaml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
+
+@functions_framework.http
+def open_weather_data_extract(request, context=None):
+
+    try:
+        response = {}
+        id = 1
+        for city_name in config['cities']:
+            lat, lon = fetch_city_coordinates(city_name['name'], OPENWEATHER_API_KEY)
+            pollution_data = fetch_air_pollution_data(lat, lon, OPENWEATHER_API_KEY)
+
+            pollution_data_with_metadata = add_metadata_to_data(pollution_data, city_name) if config['output']['metadata'] else pollution_data
+
+            file_name = generate_filename(city_name)
+
+        #     upload_to_gcs(ACCOUNT_SERVICE_KEY, GCS_BUCKET_NAME, file_name, pollution_data_with_metadata)
+            
+            # logging.info("Data ingestion and upload process completed successfully.")
+            response_data = {
+                "city_name": city_name,
+                "coordinates": {
+                    "latitude": lat,
+                    "longitude": lon
+                },
+                "pollution_data": pollution_data,
+                "pollution_data_with_metadata": pollution_data_with_metadata,
+                "file_name": file_name
+            }
+            response[id] = response_data
+            id += 1
+            
+        return str(response)
+
+    except Exception as e:
+        # logging.error(f"An error occurred: {e}")
+        return {e}
+
---
+++ gcloud_functions/utils/data_ingestion.py
@@ -0,0 +1,58 @@
+import os
+import json
+import requests
+from google.cloud import storage
+from dotenv import load_dotenv
+from datetime import datetime
+import logging
+
+
+def fetch_city_coordinates(city_name, api_key):
+    city_api = f"http://api.openweathermap.org/geo/1.0/direct?q={city_name}&limit=1&appid={api_key}"
+    response = requests.get(city_api)
+    city_data = response.json()
+    return city_data[0]['lat'], city_data[0]['lon']
+
+def fetch_air_pollution_data(lat, lon, api_key):
+    pollution_api = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
+    response = requests.get(pollution_api)
+    return response.json()
+
+def upload_to_gcs(path_to_key, bucket_name, file_name, data):
+    client = storage.Client.from_service_account_json(path_to_key)
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    blob.upload_from_string(json.dumps(data))
+    logging.info(f"File {file_name} uploaded to {bucket_name}.")
+
+def add_metadata_to_data(data, city):
+    data['city'] = city
+    data['timestamp'] = datetime.now().isoformat()
+    return data
+
+def generate_filename(city):
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    filename = f"{city}_{timestamp}.json"
+    return filename
+
+#Historia z API
+#____###
+
+# def get_air_pollution_history_data(
+#             self,
+#             lat: float,
+#             lon: float,
+#             unix_start_date: int,
+#             unix_end_date: int,
+#     ) -> dict:
+#         """
+        
+#         Get historical air pollution data for given coordinates.
+        
+#         """
+#         url = f"{self.AIR_POLLUTION_HISTORY_URL}lat={lat}&lon={lon}&start={unix_start_date}&end={unix_end_date}&appid={self.openweather_api_key}"
+#         return self.get_data_from_url(url)
+
+
+# AIR_POLLUTION_HISTORY_URL = 'http://api.openweathermap.org/data/2.5/air_pollution/history?'
+# """
